3ce543852eb1f2874a5031566390ea95d7175a55
Markus Tarn <Markus.Tarn@myjar.com>
2021-07-25 23:20:37 +0300

    Added finnish language

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
 	res.setLocale(req.cookies.localeCookie || 'en');
 	res.render('main', {
 		locals: {
@@ -11,19 +11,14 @@ router.get('/', (req, res, next) => {
 	});
 });
 
-router.get('/en', (req, res) => {
+router.get('/en', (_req, res) => {
 	res.cookie('localeCookie', 'en');
 	res.redirect('/');
 });
 
-// router.get('/ru', (req, res) => {
-// 	res.cookie('localeCookie', 'ru');
-// 	res.redirect('/');
-// });
-
-// router.get('/de', (req, res) => {
-// 	res.cookie('localeCookie', 'de');
-// 	res.redirect('/');
-// });
+router.get('/fi', (_req, res) => {
+	res.cookie('localeCookie', 'fi');
+	res.redirect('/');
+});
 
 module.exports = router;
